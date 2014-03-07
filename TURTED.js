a4adf793e3ea6b156c454e9536bd03a0861e2dbb
Peter Dietrich <peter.dietrich@smartpixelmedia.de>
2014-03-07 20:35:20 +0100

    Make sure to repeat ident after reconnect

--- TURTED.js
+++ TURTED.js
@@ -3,14 +3,18 @@ var TURTED = function (sockjs_url) {
     this.callbacks = {};
     this.nativeConnection = {};
     this.callbackQueue = [];
+    this.reconnectQueue = [];
     this.isConnected = false;
 
-    this.reconnect = function () {
+    this.connect = function (isReconnect) {
         var sockjs = new SockJS(sockjs_url);
         sockjs.onopen = function () {
             //fire queued events here
             me.isConnected = true;
             me.processQueue();
+            if (isReconnect) {
+                me.processReconnectQueue()
+            }
         }
         sockjs.onmessage = function (e) {
             //console.log("Sock gives me ", e);
@@ -41,7 +45,11 @@ var TURTED = function (sockjs_url) {
         this.nativeConnection = sockjs;
     }
 
-    this.reconnect();
+    this.reconnect = function() {
+        this.connect(true);
+    }.bind(this);
+
+    this.connect();
 }
 
 TURTED.prototype.on = function (on, f) {
@@ -53,13 +61,15 @@ TURTED.prototype.on = function (on, f) {
 
 TURTED.prototype.ident = function (id, username, token) {
     //enqueue the "ident" action until connected
-    this.enqueue(function () {
+    var f = function () {
         this.nativeConnection.send(this.encode("ident", {
             id: id,
             username: username,
             token: token
         }));
-    }.bind(this));
+    }.bind(this);
+    this.enqueue(f);
+    this.enqueueReconnect(f);
 
     //in case the call was made on an open connection, immediately process the queue again
     if (this.isConnected) {
@@ -98,14 +108,30 @@ TURTED.prototype.encode = function (type, data) {
 }
 
 TURTED.prototype.enqueue = function (f) {
-    //store the call and arguments
+    //store the callback
     this.callbackQueue.push(f);
+}
+
+TURTED.prototype.enqueueReconnect = function (f) {
+    //store the callback
+    this.reconnectQueue.push(f);
 }
 
 TURTED.prototype.processQueue = function () {
     var emergency = 100;
     while ((this.callbackQueue.length > 0) && (emergency > 0)) {
         var f = this.callbackQueue.shift();
+        if (typeof f === "function") {
+            f();
+        }
+        emergency--;
+    }
+}
+
+TURTED.prototype.processReconnectQueue = function () {
+    var emergency = 100;
+    while ((this.reconnectQueue.length > 0) && (emergency > 0)) {
+        var f = this.reconnectQueue.shift();
         if (typeof f === "function") {
             f();
         }
