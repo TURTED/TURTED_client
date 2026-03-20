8d3c57437b450e9e6f8b20608db8852b988b1fd6
Peter Dietrich <xosofox@gmail.com>
2026-03-20 12:48:28 +0100

    SOCKET fix import

--- TURTED.js
+++ TURTED.js
@@ -1,7 +1,7 @@
 // modularization conversion according to http://ifandelse.com/its-not-hard-making-your-library-support-amd-and-commonjs/
 (function(root, factory) {
     if (typeof define === "function" && define.amd) {
-        define(["io"], factory);
+        define(["socket.io-client"], factory);
     } else if (typeof module === "object" && module.exports) {
         module.exports = factory(require("socket.io-client"));
     } else {
